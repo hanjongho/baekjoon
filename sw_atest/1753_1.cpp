5b3cd354bd74dec85b19108c2b7f4fff6be5831f
한종호 <hanjongho@hanjonghoui-MacBookAir.local>
2019-12-27 05:08:11 +0900

    2019-12-27 최단경로

--- sw_atest/1753_1.cpp
+++ sw_atest/1753_1.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
-int V, E, K, u, v, w, INF = 987654321;
-vector<pair<int,int>> a[20001];
+int V, E, K, u, w, v, INF = 987654321;
+
+vector<pair<int, int>> a[20001];
 
 vector<int> Dijkstra(int vertex, int start)
 {
     vector<int> d(vertex, INF);
     d[start] = 0;
     priority_queue<pair<int, int>> pq;
-    pq.push(make_pair(start, 0));
-    
-    while (!pq.empty()){
-        int current = pq.top().first;
-        int distance = -pq.top().second;
+    pq.push(make_pair(0, start));
+    while (!pq.empty()) {
+        int distance = -pq.top().first;
+        int current = pq.top().second;
         pq.pop();
-        
-        if (d[current] < distance)
-            continue;
-        
-        for (int i = 0; i < a[current].size(); i++) {
+        if (d[current] < distance)continue;
+        for (int i = 0; i < a[current].size(); ++i) {
             int next = a[current][i].first;
-            if (d[next] > a[current][i].second + distance){
+            if (d[next] > a[current][i].second + distance) {
                 d[next] = a[current][i].second + distance;
                 pq.push(make_pair(-(a[current][i].second + distance), next));
             }
@@ -39,9 +37,10 @@ int main()
         cin >> u >> v >> w;
         a[u].push_back(make_pair(v,w));
     }
-    vector<int> dist = Dijkstra(V+1, K);
     
-    for (int i = 1; i <= V; i++) {
+    vector<int>dist = Dijkstra(V+1, K);
+    
+    for (int i = 1; i <= V; ++i) {
         if (dist[i] == INF)
             cout << "INF\n";
         else
