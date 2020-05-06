eb336f8ae04039def1cfb5cd786104bf2135093e
hanjongho <lgwl81@gmail.com>
2020-05-07 02:29:28 +0900

    New Project

---
+++ solved/solved/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  solved
+//
+//  Created by 한종호 on 07/05/2020.
+//  Copyright © 2020 hanjongho. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
