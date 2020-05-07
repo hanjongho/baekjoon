34fed813077a5bead66d89a4b1efac9604387505
hanjongho <lgwl81@gmail.com>
2020-05-07 22:24:30 +0900

    2020 05 08 Bellman-ford

---
+++ solved/algorithm/bellman-ford/11657.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#define INF 10001
+using namespace std;
+
+int N, M, A, B, C;
+vector<pair<int, int>> v[501];
+bool cycle = false;
+int dist[501] = {INF,};
+
+void bellman_ford(){
+    for(int i = 1; i <= N; i++) { // 시작점
+         for(int j = 1; j <= N; j++) { // 기준점
+             for(int k = 0; k < v[j].size(); k++) { // 기준점 정보 다 털기
+                 int dest = v[j][k].first;
+                 int cost = v[j][k].second;
+                 if (dist[j] != INF && j != dest && dist[dest] > cost + dist[j]) {
+                     dist[dest] = min(dist[dest], cost + dist[j]); //최소값으로 갱신
+                     if (i == N)
+                         cycle = true;
+                 }
+             }
+         }
+     }
+}
+
+int main(){
+   
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cin >> N >> M;
+    while(M--){
+        cin >> A >> B >> C;
+        v[A].push_back(make_pair(B, C));
+    }
+
+    for (int i = 1; i <= N; i++)
+        dist[i] = INF;
+    
+    dist[1] = 0;
+
+    bellman_ford();
+
+    if(cycle)
+        cout << -1 << "\n";
+    else{
+        for (int i = 2; i <= N; i++){
+            if (dist[i] == INF)
+                cout << -1 << "\n";
+            else
+                cout << dist[i] << "\n";
+        }
+    }
+    return 0;
+}
