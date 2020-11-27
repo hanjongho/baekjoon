f0ae1629542ee3fd7da428babd18bc9a22784fd4
hanjongho <lgwl81@gmail.com>
2020-11-28 03:10:16 +0900

    upload

--- solved/algorithm/divideandconquer/2261.cpp
+++ solved/algorithm/divideandconquer/2261.cpp
@@ -10,7 +10,7 @@ struct Point {
 };
  
 int dist(Point &p, Point &q){
-    return (p.x-q.x)*(p.x-q.x)+(p.y-q.y)*(p.y-q.y);
+    return (p.x - q.x) * (p.x - q.x) + (p.y - q.y) * (p.y - q.y);
 }
 
 bool cmpx(Point &p1, Point &p2){
@@ -28,42 +28,41 @@ int closest_pair(vector<Point>::iterator it, int n) {
         return min({dist(it[0], it[1]), dist(it[1], it[2]), dist(it[2], it[0])});
      
     int line = (it[n/2 - 1].x + it[n/2].x) / 2;
-    int d = min(closest_pair(it, n/2), closest_pair(it + n/2, n/2)); // n - n/2 -> n/2
+    int d = min(closest_pair(it, n/2), closest_pair(it + n/2, n/2));
     
     vector<Point> mid;
     mid.reserve(n);
-     
-    for(int i=0; i<n; i++){
+
+    for(int i = 0; i < n; i++) {
         int t = line - it[i].x;
-        if (t*t < d) // 제곱하면 좌우상관없음
+        if (t * t < d) // 제곱하면 좌우상관없음
             mid.push_back(it[i]);
     }
-    
+
     sort(mid.begin(), mid.end(), cmpy); // y축 정렬
     
     int mid_sz = mid.size();
     for (int i = 0; i < mid_sz - 1; i++)
-        for (int j = i + 1; j < mid_sz && (mid[j].y - mid[i].y)*(mid[j].y - mid[i].y) < d; j++)
+        for (int j = i + 1; j < mid_sz && (mid[j].y - mid[i].y) < d; j++)
             d = min(d, dist(mid[i], mid[j]));
-    
-    return d;
+    return (d);
 }
  
  
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(0);
+    cout.tie(0);
  
     int n;
     cin >> n;
-     
+
     vector<Point> points(n);
     for (vector<Point>::iterator it = points.begin(); it != points.end(); it++)
         cin >> it->x >> it->y;
      
     sort(points.begin(), points.end(), cmpx); // x축 정렬
-     
     cout << closest_pair(points.begin(), n);
-     
-    return 0;
+    return (0);
 }
+
---
+++ solved/readme.md
@@ -0,0 +1,18 @@
+```javascript
+
+// Recursive
+int fib(int n)
+{
+    if (n <= 1)
+        return (n);
+    return fib(n-1) + fib(n-2);
+}
+
+// DP
+fib[0] = 0;
+fib[1] = 1;
+
+for(int i = 2; i <= n; i++)
+    fib[i] = fib[i-1] + fib[i-2];
+return fib[n];
+```
